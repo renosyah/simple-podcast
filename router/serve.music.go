260f1b216bcd731b92d1e7262adddac151b36813
renosyah <syahputrareno975@gmail.com>
2020-11-02 15:06:00 +0700

    test streaming file, not work

--- router/serve.music.go
+++ router/serve.music.go
@@ -26,6 +26,58 @@ func HandlerFileMusic(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// f, errF := os.Open(m.FilePath)
+	// if errF != nil {
+	// 	w.WriteHeader(http.StatusNotFound)
+	// 	fmt.Println(errF)
+	// 	return
+	// }
+	// defer f.Close()
+
+	// fi, errFs := f.Stat()
+	// if errFs != nil {
+	// 	w.WriteHeader(http.StatusNotFound)
+	// 	fmt.Println(errFs)
+	// 	return
+	// }
+
+	// w.Header().Set("Content-Type", "audio/mpeg")
+	// w.Header().Set("Cache-Control", "no-cache")
+	// w.Header().Set("Connection", "keep-alive")
+	// w.Header().Set("Content-Length", fmt.Sprint(fi.Size()))
+
+	// bfr := bufio.NewReader(f)
+	// buf := make([]byte, 0, 4*1024)
+
+	// for {
+	// 	n, err := bfr.Read(buf[:cap(buf)])
+	// 	buf = buf[:n]
+	// 	if n == 0 {
+	// 		if err == nil {
+	// 			continue
+	// 		}
+	// 		if err == io.EOF {
+	// 			break
+	// 		}
+	// 		return
+	// 	}
+
+	// 	w.Write(buf)
+	// 	flusher, ok := w.(http.Flusher)
+	// 	if !ok {
+	// 		w.WriteHeader(http.StatusInternalServerError)
+	// 		return
+	// 	}
+	// 	flusher.Flush()
+
+	// 	if err != nil && err != io.EOF {
+	// 		w.WriteHeader(http.StatusInternalServerError)
+	// 		return
+	// 	}
+
+	// }
+
 	w.Header().Set("Content-Type", "audio/mpeg")
+	w.Header().Set("Cache-Control", "no-cache")
 	http.ServeFile(w, r, m.FilePath)
 }
